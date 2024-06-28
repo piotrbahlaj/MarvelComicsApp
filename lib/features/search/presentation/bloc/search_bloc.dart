057b85823afbdda76bd17dead4b002686f80143b
Piotr Bahlaj <piotr.bahlaj@o2.pl>
2024-06-28 21:16:11 +0200

    created a timer for search bloc

--- lib/features/search/presentation/bloc/search_bloc.dart
+++ lib/features/search/presentation/bloc/search_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:marvel_comics_app/services/marvel_service.dart';
@@ -7,30 +9,54 @@ part 'search_state.dart';
 
 class SearchBloc extends Bloc<SearchEvent, SearchState> {
   final MarvelService marvelService;
+  Timer? _searchTimer;
+  String _latestQuery = '';
+
   SearchBloc(this.marvelService) : super(SearchInitial()) {
     on<SearchQueryChanged>(_onSearchQueryChanged);
   }
+
   void _onSearchQueryChanged(
       SearchQueryChanged event, Emitter<SearchState> emit) async {
     final query = event.query;
+    if (_searchTimer?.isActive == false) {
+      _searchTimer?.cancel();
+    }
+    _searchTimer = Timer(
+      const Duration(milliseconds: 400),
+      () async {
+        final query = event.query;
+        _latestQuery = query;
+      },
+    );
+
     if (query.isEmpty) {
       emit(SearchInitial());
       return;
     }
+
     emit(SearchLoading());
     try {
       final results = await marvelService.searchComics(query);
-      if (results.isEmpty) {
-        emit(
-          const SearchError(
-            'There is no comic book with that name in our library. Check the spelling and try again',
-          ),
-        );
-      } else {
-        emit(SearchLoaded(results));
+      if (query == _latestQuery) {
+        if (results.isEmpty) {
+          emit(
+            const SearchError(
+              'There is no comic book with that name in our library. Check the spelling and try again',
+            ),
+          );
+        } else {
+          emit(SearchLoaded(results));
+        }
       }
     } catch (e) {
       emit(const SearchError('Failed to load search results'));
     }
+  }
+
+  @override
+  Future<void> close() {
+    _searchTimer?.cancel();
+    return super.close();
   }
 }
